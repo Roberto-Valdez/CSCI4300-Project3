165a41541baf0ab81890caa0a62a2cb1595f2315
Roberto-Valdez <43326202+Roberto-Valdez@users.noreply.github.com>
2019-11-30 18:10:25 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,29 @@
-def application(env, start_response):
-    start_response('200 OK', [('Content-Type', 'text/html')])
-    response = " "
-    return response.encode()
+from flask import Flask, render_template
+from flask_mysqldb import MYSQL
+
+app = Flask(__name__)
+app.config['MYSQL_HOST'] = '127.0.0.1:4800' #replace with your own nginx host number                                                                                 
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'password' #replace with your own mysql password                                                                                      
+app.config['MYSQL_DB'] = 'classicmodels'
+mysql = MySQL(app)
+
+@app.route("/p3/", methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        employeeID = request.form
+        id = employeeID['employeeID'] #stores desired employeeID to be used in the mysql query later                                                                 
+	cur = mysql.connection.cursor()
+	tableResults = cur.execute("MYSQL QUERY") #insert query here and use where id = employeeID from up above                                                     
+	if tableResults > 0:  #check to see if there are results associated with the employeeID number                                                               
+            row_headers = [x[0] for x in cur.description]
+            tableDetails = cur.fetchall()
+            json_data = []
+	    for result in tableDetails:
+		json_data.append(dict(zip(row_headers,result)))
+	    return json.dumps(json_data) #returns json data, still need to figure out how to link to HTML and possibly parse using jQuery.getJSON() functions        
+
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
