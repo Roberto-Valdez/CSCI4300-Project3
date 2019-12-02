b4da2b8fc04334b5e8bf7fa767629f54077557d0
Roberto-Valdez <43326202+Roberto-Valdez@users.noreply.github.com>
2019-12-02 17:06:13 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,38 +1,36 @@
-from flask import Flask, render_template, request
-from flask_mysqldb import MySQL
+
+import mysql.connector
+from pprint import pprint
 import json
+import decimal
 
-app = Flask(__name__)
-app.config['MYSQL_HOST'] = '127.0.0.1:4800' #replace with your own nginx host number                                                                                              
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'password' #replace with your own mysql password                                                                                                   
-app.config['MYSQL_DB'] = 'classicmodels'
-mysql = MySQL(app)
-
-@app.route("/p3/", methods=['GET', 'POST'])
-def index():
-    print("test1")
-    if request.method == 'POST':                                                                                                                               
-        employeeID = request.form                                                                                                                                                
-        id = employeeID['employeeID'] #stores desired employeeID to be used in the mysql query later                                                                             
-        cur = mysql.connection.cursor()
-        tableResults = cur.execute("SELECT employeeNumber, orderdetails.orderNumber, quantityOrdered*priceEach AS Sales FROM customers JOIN orders ON customers.customerNumber = \
-orders.customerNumber JOIN orderdetails ON orders.orderNumber = orderdetails.orderNumber JOIN employees ON employees.employeeNumber = customers.salesRepEmployeeNumber;") #insert\
- query here and use where id = employeeID from up above                                                                                                                           
-        print("test2")
-        if tableResults > 0:  #check to see if there are results associated with the employeeID number                                                                            
-            rows = [x[0] for x in cur.description]
-            tableDetails = cur.fetchall()
-            json = []
-            for result in tableDetails:
-                json_data.append(dict(zip(rows,result)))
-            jsonString = json.dumps(json) #returns json data, still need to figure out how to link to HTML and possibly parse using jQuery.getJSON() functions                    
-            print(jsonString)
-            print("test3")
-
-    return render_template('index.html')
-
-if __name__ == '__main__':
-    app.run(debug=True)
+creds = { 'user' : 'FOO',
+          'database' : 'classicmodels',
+          'password' : 'FOOp4ss$',
+          'auth_plugin' : 'mysql_native_password'}
 
 
+def application (env, start_response):
+    if env['REQUEST_METHOD'] == 'GET':
+        start_response('200 OK', [('Content-Type', 'application/json')])
+        body = env['QUERY_STRING']
+        id = body.split('=')[-1]
+        id = int(id)
+
+        cnx = mysql.connector.connect(**creds)
+
+        cursor = cnx.cursor(dictionary=True)
+
+        cursor.execute('SELECT customers.salesRepEmployeeNumber AS employeeNumber, orderdetails.orderNumber, productCode, quantityOrdered, priceEach, quantityOrdered*priceEach as total FROM customers JOIN orders ON customers.customerNumber = orders.customerNumber JOIN orderdetails ON orders.orderNumber = orderdetails.orderNumber WHERE customers.salesRepEmployeeNumber = id;')
+
+        values = cursor.fetchall()
+
+        def decimal_default(obj):
+            if isinstance(obj, decimal.Decimal):
+                return float(obj)
+            raise TypeError
+
+#        html = open("jq5.html")
+#        return html.read()
+       
+        return(json.dumps(values, default=decimal_default))
